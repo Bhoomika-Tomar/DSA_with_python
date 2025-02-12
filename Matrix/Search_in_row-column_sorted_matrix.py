025b410193cf6542e358c77792c4f97d4e21829c
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-02-12 16:36:32 +0530

    First commit

---
+++ Matrix/Search_in_row-column_sorted_matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+	def matSearch(self, mat, x):
+		# Complete this function
+		
+		#Calculating no of rows and columns
+		n = len(mat)
+		m = len(mat[0])
+		#Begin searching with element in 1st row and last
+		#column
+		i = 0
+		j = m -1
+		
+		while i < n and j >= 0:
+		   
+		    #If k is greater than mat[i][j] then search in down
+		    #direction i.e. column
+		    if x > mat[i][j]:
+		        i = i + 1;
+		        
+		    #If k is less than mat[i][j] then search in left
+		    #direction i.e. row
+		    elif x < mat[i][j]:
+		        j = j - 1;
+		    
+		    #If k is found at mat[i][j]
+		    else:
+		        return True
+			        
+		return False
+		    
