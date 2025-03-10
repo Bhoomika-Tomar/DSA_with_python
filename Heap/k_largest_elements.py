c3afa2ac7d504fb72acd09919929eaba1c31756e
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-03-10 22:29:03 +0530

    First commit

---
+++ Heap/k_largest_elements.py
@@ -0,0 +1,22 @@
+import heapq
+
+class Solution:
+	def kLargest(self, arr, k):
+		# Your code here
+		
+		#Creating a min heap for k elements
+		minH = arr [ : k]
+		
+		heapq.heapify (minH)
+		
+		for x in arr[k : ]:
+		    if x > minH[0]:
+		        heapq.heapreplace (minH, x)
+		        
+		res = []
+		while minH:
+		    res.append (heapq.heappop(minH))
+		     
+		res.reverse()
+		 
+		return res
