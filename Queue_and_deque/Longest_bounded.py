4c3db2db3a72cb5bbe2471bd840d94d4b04f2141
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-03-22 23:36:50 +0530

    First commit

---
+++ Queue_and_deque/Longest_bounded.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class Solution:
+    def longestSubarray(self, arr, x):
+        
+        n = len(arr)
+        minQueue = deque()
+        maxQueue = deque()
+        
+        start = end = 0
+        resStart = resEnd = 0
+        
+        while end < n:
+            while minQueue and arr[minQueue[-1]] > arr[end]:
+                minQueue.pop()
+                
+            while maxQueue and arr[maxQueue[-1]] < arr[end]:
+                maxQueue.pop()
+                
+            minQueue.append(end)
+            maxQueue.append(end)
+            
+            while arr[maxQueue[0]] - arr[minQueue[0]] > x:
+                
+                if start == minQueue[0]:
+                    minQueue.popleft()
+                if start == maxQueue[0]:
+                    maxQueue.popleft()
+                
+                start += 1
+                
+            if end - start > resEnd - resStart:
+                resEnd, resStart = end, start
+                
+            end += 1
+            
+        return arr[resStart : resEnd + 1]
