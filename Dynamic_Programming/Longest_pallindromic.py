765fcd6fb8eba0f562d92cb146eed1140652378c
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-04-05 22:28:46 +0530

    First commit

---
+++ Dynamic_Programming/Longest_pallindromic.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def longestPalinSubseq(self, s):
+        # code here
+        
+        n = len(s)
+        
+        curr = [0] * n
+        prev = [0] * n
+        
+        for i in range (n-1, -1, -1):
+            curr[i] = 1
+            
+            for j in range (i+1, n):
+                if s[i] == s[j]:
+                    curr[j] = prev[j-1] + 2
+                
+                else:
+                    curr[j] = max(prev[j], curr[j-1])
+        
+            prev = curr[ : ] 
+            
+        return curr[n - 1]
+            
+        
+        
+    
