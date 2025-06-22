ec2291635f4ae32cf34d82e69702cc774fb02667
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-06-22 17:08:53 +0530

    First commit

--- Arrays/move_all_zeros_to_end.py
+++ Arrays/move_all_zeros_to_end.py
@@ -1,8 +1,15 @@
+
 class Solution:
-	def pushZerosToEnd(self,arr):
-    	# code here
-        count = 0
+    def pushZerosToEnd(self, arr: list[int]) -> None:
+        count = 0  # position to place the next non-zero element
+
         for i in range(len(arr)):
-           if arr[i]!= 0:
-               arr[i],arr[count]=arr[count],arr[i]
-               count = count + 1
+            if arr[i] != 0:
+                arr[i], arr[count] = arr[count], arr[i]
+                count += 1
+
+
+arr = list(map(int, input("Enter array: ").split()))
+sol = Solution()
+sol.pushZerosToEnd(arr)
+print("Array after pushing zeros to end:", arr)
