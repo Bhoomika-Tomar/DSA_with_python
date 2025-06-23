838b5cc557e4912c44fd8331e89d2628be26ec27
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-06-23 15:31:17 +0530

    First commit

--- Arrays/rotate_array.py
+++ Arrays/rotate_array.py
@@ -1,15 +1,27 @@
 class Solution:
-    #Function to rotate an array by d elements in counter-clockwise direction. 
+    #Function to rotate an array by d elements in counter-clockwise direction(left). 
     def rotateArr(self,arr,d):
         n= len(arr)
-        d = d%n
-        
-        reverse (arr,0,d-1)
-        reverse (arr,d,n-1)
-        reverse (arr,0,n-1)
+        d = d%n  #for counter clockwise
+
+        #d = n - (d%n) #for clockwise (right)
+
+        self.reverse (arr, 0, d-1)
+        self.reverse (arr, d, n-1)
+        self.reverse (arr, 0, n-1)
+
+
+    def reverse (self, arr, start, end):
+        while start < end:
+
+            arr[start],arr[end] = arr[end],arr[start]
+            start = start + 1
+            end = end - 1
+
     
-def reverse (arr,start,end):
-    while start<end:
-        arr[start],arr[end]=arr[end],arr[start]
-        start = start+1
-        end = end-1
+arr = list(map(int, input("Enter array: ").split()))
+d = int(input("Enter the no."))
+sol = Solution()
+sol.rotateArr(arr, d)
+print ("Array after rotation",arr)
+
