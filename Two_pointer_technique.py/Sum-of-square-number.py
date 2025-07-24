b6a29cab48347f0a60c9cf6628ae13b5b61de725
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-07-24 23:31:54 +0530

    First commit

---
+++ Two_pointer_technique.py/Sum-of-square-number.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        
+        #a^2 + b^2 = c
+
+        left = 0
+        right = int(math.isqrt(c))   #Largest possible value for b
+
+        while left < right:
+            value = left * left + right * right
+
+            if value == c:
+                return True
+
+            elif value < c:
+                left = left + 1
+
+            else:
+                right = right - 1
+
+        return False
+
+c = int (input("Enter the number"))
+sol = Solution()
+print(sol.judgeSquareSum(c))
+
+g
