07d5c1b8e0b39bb63798759d6481cbfae1a84a40
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-08-07 22:56:09 +0530

    First commit

--- Two_pointer_technique.py/Sort-array-by-parity.py
+++ Two_pointer_technique.py/Sort-array-by-parity.py
@@ -1,7 +1,7 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         left = 0
-        right = len(nums) - 1
+        right = len(nums) - 1  
 
         while left < right:
             if nums[left] % 2 > nums[right] % 2 :
