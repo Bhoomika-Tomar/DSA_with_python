7a975273fc29d659f7b5c1ad3296a9ff591b6f91
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-08-14 23:35:07 +0530

    First commit

---
+++ Slow-and-fast-pointers/Find-duplicate-numbers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicate(self, arr: List[int]) -> int:
+        #Find the intersection point
+        slow = arr[0]
+        fast = arr[0]
+
+        while True:
+            slow = arr[slow]
+            fast = arr[arr[fast]]
+
+            if slow == fast:
+                break
+
+        #Finding the intersection point again
+        slow = arr[0]
+        while slow != fast:
+            slow = arr[slow]
+            fast = arr[fast]
+
+        return slow       
