52e6c4607f5ccded7ce34a14c2df39850a891e41
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-10-18 22:27:19 +0530

    First commit

---
+++ Binary-search/Sum-closest.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+
+        nums.sort()
+        n = len(nums)
+        closest_sum = float('inf')
+
+        for i in range (n-2):
+            l = i+1
+            r = n-1
+
+            while l < r:
+                curr_sum = nums[i] + nums[l] + nums[r]
+
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum = curr_sum
+
+                if curr_sum < target:
+                    l += 1
+
+                elif curr_sum > target:
+                    r -= 1
+
+                else:
+                    return curr_sum
+
+        return closest_sum
+        
